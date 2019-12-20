b4828b77902c52d60133f11b20e248bab0f30356
GwakHyeonJun <gwakhyeonjun@gmail.com>
2019-12-20 09:58:45 +0900

    Update Sequelize Init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const port = process.env.PORT || 3000;
+const db = require('./models/index');
 const app = express();
+
+db.sequelize.sync();
 
 app.use(cors());
 app.use(morgan('dev'));
