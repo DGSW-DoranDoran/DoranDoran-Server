0eb697adf16197c78bce87ca147aef7ebeaad9af
QMCHE <39144276+QMCHE@users.noreply.github.com>
2019-12-23 16:19:17 +0900

    [FIX] API Index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
-const port = process.env.PORT || 3000;
+const colors = require('colors');
 const db = require('./models/index');
 const api = require('./api');
+const secret = require('./config/config.json').secret;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('jwt-secret', secret);
+
 db.sequelize.sync();
 
 app.use('/', api);
 
-app.listen(port, () => {
-    console.log(`server started at ${port}`);
+const listener = app.listen(3001, () => {
+    console.log(colors.white("Server Started at " + listener.address().port));
 });
