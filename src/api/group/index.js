d9a58fe7e65b256ed84c8165eef1528f4dfd2498
QMCHE <39144276+QMCHE@users.noreply.github.com>
2019-12-23 19:42:43 +0900

    [UPDATE] Group Modify & Delete

--- src/api/group/index.js
+++ src/api/group/index.js
@@ -4,5 +4,7 @@ const groupCtrl = require('./groupCtrl');
 router.get('/groups', groupCtrl.getGroups);
 router.get('/info', groupCtrl.getGroupInfo);
 router.post('/create', groupCtrl.createGroup);
+router.put('/modify', groupCtrl.modifyGroup);
+router.delete('/delete', groupCtrl.delete);
 
 module.exports = router;
