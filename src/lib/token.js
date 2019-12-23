e03365bb85d411b80ac8150c604e46cb4fbebb57
QMCHE <39144276+QMCHE@users.noreply.github.com>
2019-12-23 16:18:43 +0900

    [FIX] JsonWebToken

--- src/lib/token.js
+++ src/lib/token.js
@@ -1,46 +1,13 @@
-require('dotenv').config();
-
 const jwt = require('jsonwebtoken');
+const secretCode = require('../config/config.json').secret;
 
-const secret = require('../config/secretToken.json');
+exports.encodeToken = (id, name) => jwt.sign({
+        id: id,
+        name: name
+    },
+    secretCode,
+    {
+        expiresIn: "60m"
+    });
 
-// expiresIn format 7 days, 10h, 7d, 3min, 100 => 100ms
-
-// Create Token
-exports.createToken = async (member) => {
-  const payload = {
-    member,
-  };
-  const option = { expiresIn: '5 days', issuer: 'dgswbamboo.com', subject: 'token' };
-  console.log(payload);
-  
-  try {
-    return await jwt.sign(payload, secret.key, option);
-  } catch (error) {
-    throw error;
-  }
-};
-
-// Create Refresh Token
-exports.createRefreshToken = async (member) => {
-  const payload = {
-    member,
-  };
-  const option = { expiresIn: '7 days', issuer: 'dgswbamboo.com', subject: 'refreshToken' };
-
-  try {
-    return await jwt.sign(payload, secret.key, option);
-  } catch (error) {
-    throw error;
-  }
-};
-
-// Verify Token
-exports.verifyToken = async (token) => {
-  try {
-    return await jwt.verify(token, secret.key);
-  } catch (error) {
-    console.log(error.message);
-    throw error;
-  }
-};
+exports.decodeToken = (token) => jwt.verify(token, secretCode);
