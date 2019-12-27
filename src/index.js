32f13b69d451f39f2ed25811f46b3a6d88d63b6c
QMCHE <39144276+QMCHE@users.noreply.github.com>
2019-12-27 11:17:29 +0900

    [FIX] Save File Name

--- src/index.js
+++ src/index.js
@@ -5,23 +5,32 @@ const colors = require('colors');
 const db = require('./models/index');
 const api = require('./api');
 const secret = require('./config/config.json').secret;
+const middleware = require('./middleware/time');
 
 const app = express();
-const upload = multer({dest: './public'});
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({storage: storage});
 
 app.use(cors());
-app.use(express.raw());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(upload.single('image'));
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
 app.set('jwt-secret', secret);
 
 db.sequelize.sync();
 
+app.use(middleware.checkTime);
 app.use('/', api);
 
-const listener = app.listen(3000, () => {
+const listener = app.listen(3001, () => {
     console.log(colors.white("Server Started at " + listener.address().port));
 });
