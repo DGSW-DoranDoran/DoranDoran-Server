f012b00b54b464fcf17454d087a66a92b45fa4df
KimGeeWon <kjw403300@gmail.com>
2020-03-11 18:31:50 +0900

    [upload] upload
    .

--- src/api/auth/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authCtrl = require('./authCtrl');
-
-router.post('/login', authCtrl.login);
-router.post('/register', authCtrl.register);
-router.get('/info', authCtrl.findInfo);
-
-module.exports = router;
--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const auth = require('./auth');
-const group = require('./group');
-
-router.use('/auth', auth);
-router.use('/group', group);
-
-module.exports = router;
